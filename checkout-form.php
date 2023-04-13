8b33d152739644874d73e131a2e065d8edec975a
Osvaldo <torquati.osvaldo@gmail.com>
2023-04-13 01:06:40 -0300

    Create checkout-form.php
    Starting the checkou form

---
+++ checkout-form.php
@@ -0,0 +1,31 @@
+<?php
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+include_once './Connection.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <title>Formulário de Pagamento</title>
+</head>
+<body
+    <?php
+          $query_products = "SELECT id, name, description, price, image FROM products WHERE id=:id LIMIT 1";
+          $result_products = $conn->prepare($query_products);
+          $result_products->bindParam(':id', $id, PDO::FETCH_ASSOC);
+          $result_products->execute();
+          $row_product = $result_products->fetch(PDO::FETCH_ASSOC);
+          extract($row_product);
+          $price_rise = ($price*1.5);
+          //var_dump($row_product);
+    ?>
+  <div class="container">
+
+  </div>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
