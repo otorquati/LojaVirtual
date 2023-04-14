b6e49a8361e12e5e62cd322878e197d97294c434
Osvaldo <torquati.osvaldo@gmail.com>
2023-04-14 01:36:28 -0300

    Checkout-form
    Devlopment of Checout form

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include_once './Connection.php';
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="shortcut icon" href="./img/icons/favicon-32x32.png" type="image/x-icon">
   <title>Loja Virtual</title>
 </head>
 <body>
