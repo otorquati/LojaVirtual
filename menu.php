8fb03d61f92ae2b74aecc88c29302e1f2f67b0c4
Osvaldo Torquati <62728159+otorquati@users.noreply.github.com>
2023-04-29 15:49:21 -0300

    Update menu.php
    Fixed the toggle menu icon

--- menu.php
+++ menu.php
@@ -2,8 +2,8 @@
     <div class="container">
       <a class="navbar-brand" href="index.php">Torquality</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
       </button>
-      <span class="navbar-toggler-icon"></span>
       <div class="collapse navbar-collapse" id="navbarsExample07">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
